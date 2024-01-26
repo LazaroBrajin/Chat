a2df6ca3255181bfcc36c450d56676f7937a2285
dev <lbrajin@yahoo.com>
2024-01-26 09:22:41 -0500

    Primer commit

---
+++ App.js
@@ -0,0 +1,18 @@
+// App.js
+import React from "react";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import Chat from "./components/Chat";
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Firebase Chat App</h1>
+      <Login />
+      <Logout />
+      <Chat />
+    </div>
+  );
+};
+
+export default App;
